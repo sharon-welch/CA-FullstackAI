a602edf53702bb594b786ad11707b8a5c0d3601f
Sharon Welch <sharon.michelle.welch@gmail.com>
2026-07-07 10:40:43 -0600

    added array calculator

--- hw2_js_calculator/script.js
+++ hw2_js_calculator/script.js
@@ -16,3 +16,47 @@ function calculator(num1, num2, operator) {
 
 calculator(10,2,"multiply");
 console.log(3+2);
+
+// create an for loop to go through array and perform each calculation 
+const problems = [
+  [10, 5, "add"],
+  [10, 5, "subtract"],
+  [10, 5, "multiply"],
+  [10, 5, "divide"]
+];
+
+// fucntion that goes through 2D array and performs calculations
+function arrCalc(array) {
+  let num1 = 0;
+  let num2 = 0;
+  let operator = undefined;
+
+  for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array[i].length; j++) {
+          // console.log(array[i][j]);  
+      }
+      num1 = array[i][0];
+      num2 = array[i][1];
+      operator = array[i][2];
+      // console.log(num1);
+      // console.log(num2); 
+      // console.log(operator); 
+    
+      if (operator === "add") {
+          console.log(num1 + num2);
+      } else if (operator === "subtract"){
+          console.log(num1 - num2);
+      } else if (operator === "multiply") {
+          console.log(num1 * num2);
+      } else if (operator === "divide") {
+          console.log(num1 / num2)
+      } else {
+          console.log("Invalid operator.")
+      }
+  }
+  // console.log(num1);
+  // console.log(num2); 
+  // console.log(operator); 
+}
+
+arrCalc(problems);
